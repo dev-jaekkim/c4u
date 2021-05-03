1609ad795ec8b7c8768e3a92c5ba98f293694deb
jaekyungkim <dev.jaekyungkim@gmail.com>
2021-05-03 21:05:25 +0900

    Spring Security Setting 1. create login handler 2. create logout handler 3. create login.jsp 4. edit hyeongdon.js

--- c4uback/src/main/java/com/my/control/LoginStatus.java
+++ c4uback/src/main/java/com/my/control/LoginStatus.java
@@ -1,26 +1,71 @@
 package com.my.control;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LoginStatus extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("application/json;charset=utf-8");
-		PrintWriter out = response.getWriter();
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class LoginStatus {
+
+	@GetMapping(value = "/loginstatus", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	protected Map<String, Object> login(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Map<String, Object> map = new HashMap<>();
 		HttpSession session = request.getSession();
-		String loginedId = (String)session.getAttribute("loginInfo");
-		if(loginedId != null) {
-			out.print("{\"status\" : 1}");
-		}else {
-			out.print("{\"status\" : -1}");
+		String loginedId = (String) session.getAttribute("loginInfo");
+
+		if (loginedId != null) {
+			log.debug("로그인O, 권한 O");
+			map.put("status", 1);
+		} else {
+			log.debug("로그인X, 권한 X");
+			map.put("status", -1);
 		}
+		return map;
 	}
+
+	@GetMapping(value = "/adminloginstatus", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	protected Map<String, Object> adminLogin(HttpServletRequest request, HttpServletResponse response, 
+											Authentication auth)
+			throws ServletException, IOException {
+
+		Map<String, Object> map = new HashMap<>();
+		HttpSession session = request.getSession();
+		String loginedId = (String) session.getAttribute("loginInfo");
+
+		List<GrantedAuthority> authorities = (List<GrantedAuthority>) auth.getAuthorities();
+        String strAuth = authorities.get(0).getAuthority();
+		
+        log.debug("strAuth: "+strAuth);
+        
+		if (strAuth.equals("ROLE_ADMIN")) {
+			log.debug("로그인O, 권한 O");
+			map.put("adminRole", 1);
+		} else {
+			log.debug("로그인X, 권한 X");
+			map.put("adminRole", -1);
+		}
+		return map;
+	}
+
 }
--- c4uback/src/main/java/com/my/controller/LoginController.java
+++ c4uback/src/main/java/com/my/controller/LoginController.java
@@ -1,17 +1,52 @@
 package com.my.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
+
+@Controller
 @Log4j
 public class LoginController {
-	@GetMapping("/myLogin")
-	public void getLogin(HttpServletResponse response) throws IOException {
-		log.debug("로그인 !");
-//		response.sendRedirect("http://localhost:8888/c4ufront/login.html");
+	@RequestMapping("/accessError")
+	public void accessError(Authentication auth, Model model) {
+		//인증된 유저의 이름을 가져올 수 있다. 
+		if(auth != null) {
+			String userName = auth.getName();
+			model.addAttribute("msg", userName+"은 접근불가한 사이트입니다.");
+		}
 	}
+	
+	@GetMapping("/myLogin")
+	/**
+	 * 인증실패된 후 또는 로그아웃후에 로그인페이지로 리다이렉트된다. 이때 queryString이 전달됨
+	 * error         logout
+	 */
+	public void getMyLogin(String error, String logout, Model model) {
+		if(error != null) { //인증실패인 경우 전달된 요청전달데이터 error
+			//error내용을 view에 전달
+			//model.addAttribute("error", "인증실패되었습니다"); //?
+			model.addAttribute("error", error);
+			
+		}
+		if(logout != null) { //로그아웃된 경우 
+			model.addAttribute("logout", "로그아웃되었습니다");
+		}
+		log.debug("로그인 요청");
+	}
+	
+	@GetMapping("/myLogout")
+	public void getMyLogout() {}
+	
+	@PostMapping("/myLogout")
+	public void postMyLogout() { //실제로 하는 일은 없고 viewer와 연결만 하면 된다. 
+	}
+	
+//	@PostMapping("/myLogin")
+//	public void postMyLogin() {
+//	}
 }
